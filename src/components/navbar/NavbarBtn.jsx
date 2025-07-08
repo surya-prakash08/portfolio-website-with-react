e259bab5c0ccf7f11be424228267b29ace97f552
Surya <spsurya392@gmail.com>
2025-07-08 12:59:41 +0530

    resume code updated

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -1,10 +1,13 @@
 
 import { LuArrowDownRight } from "react-icons/lu";
 const NavbarBtn = () => {
+  const resumePath = "/Surya_Prakash_Infosys_Resume.pdf";
   return (
     <a 
-    href="/public"
-    download="Surya_Prakash_Infosys_Resume.pdf"
+    href={resumePath}
+
+    download
+    target="_blank"
     className='px-4 py-2 rounded-full text-m font-bold text-white border-cyan border flex items-center gap-1 bg-gradient-to-r from-cyan to-orange hover:border-orange hover:scale-110 transition-all duration-500 hover:shadow-cyanShadow'>Download Resume
     <div className='sm:hidden md:block'>
       <LuArrowDownRight />
--- src/components/navbar/NavbarLinks.jsx
+++ src/components/navbar/NavbarLinks.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 // import { Link } from "react-router-dom";
 import {Link} from "react-scroll";
 const links = [
